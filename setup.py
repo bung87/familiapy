e6c8d1123c98826f1da0f03c9709cdc21a5f5c35
zhoupeng <zhoupeng@xjgreat.com>
2018-02-06 15:50:01 +0800

    keep simple clean otherwise cause 1:gcc-6: error: unrecognized command line option '-stdlib=libstdc++'; did you mean '-static-libstdc++'? 2:ImportError: dynamic module does not define module export function (PyInit_familia)

--- setup.py
+++ setup.py
@@ -70,57 +70,57 @@ ext_modules = [
 
 # As of Python 3.6, CCompiler has a `has_flag` method.
 # cf http://bugs.python.org/issue26689
-def has_flag(compiler, flagname):
-    """Return a boolean indicating whether a flag name is supported on
-    the specified compiler.
-    """
-    import tempfile
-    with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
-        f.write('int main (int argc, char **argv) { return 0; }')
-        try:
-            compiler.compile([f.name], extra_postargs=[flagname])
-        except setuptools.distutils.errors.CompileError:
-            return False
-    return True
+# def has_flag(compiler, flagname):
+#     """Return a boolean indicating whether a flag name is supported on
+#     the specified compiler.
+#     """
+#     import tempfile
+#     with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
+#         f.write('int main (int argc, char **argv) { return 0; }')
+#         try:
+#             compiler.compile([f.name], extra_postargs=[flagname])
+#         except setuptools.distutils.errors.CompileError:
+#             return False
+#     return True
 
 
-def cpp_flag(compiler):
-    """Return the -std=c++[11/14] compiler flag.
-    The c++14 is prefered over c++11 (when it is available).
-    """
-    if has_flag(compiler, '-std=c++14'):
-        return '-std=c++14'
-    elif has_flag(compiler, '-std=c++11'):
-        return '-std=c++11'
-    else:
-        raise RuntimeError('Unsupported compiler -- at least C++11 support '
-                           'is needed!')
+# def cpp_flag(compiler):
+#     """Return the -std=c++[11/14] compiler flag.
+#     The c++14 is prefered over c++11 (when it is available).
+#     """
+#     if has_flag(compiler, '-std=c++14'):
+#         return '-std=c++14'
+#     elif has_flag(compiler, '-std=c++11'):
+#         return '-std=c++11'
+#     else:
+#         raise RuntimeError('Unsupported compiler -- at least C++11 support '
+#                            'is needed!')
 
 
-class BuildExt(build_ext):
-    """A custom build extension for adding compiler-specific options."""
-    c_opts = {
-        'msvc': ['/EHsc'],
-        'unix': [],
-    }
+# class BuildExt(build_ext):
+#     """A custom build extension for adding compiler-specific options."""
+#     c_opts = {
+#         'msvc': ['/EHsc'],
+#         'unix': [],
+#     }
 
-    if sys.platform == 'darwin':
-        c_opts['unix'] += ['-stdlib=libstdc++','-flat_namespace',"-lstdc++",'-mmacosx-version-min=10.7']
+#     if sys.platform == 'darwin':
+#         c_opts['unix'] += ['-flat_namespace',"-lstdc++",'-mmacosx-version-min=10.7']
 
-    def build_extensions(self):
-        ct = self.compiler.compiler_type
-        opts = self.c_opts.get(ct, [])
-        opts.append("-fPIC")
-        if ct == 'unix':
-            opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
-            opts.append(cpp_flag(self.compiler))
-            if has_flag(self.compiler, '-fvisibility=hidden'):
-                opts.append('-fvisibility=hidden')
-        elif ct == 'msvc':
-            opts.append('/DVERSION_INFO=\\"%s\\"' % self.distribution.get_version())
-        for ext in self.extensions:
-            ext.extra_compile_args = opts
-        build_ext.build_extensions(self)
+#     def build_extensions(self):
+#         ct = self.compiler.compiler_type
+#         opts = self.c_opts.get(ct, [])
+#         opts.append("-fPIC")
+#         if ct == 'unix':
+#             opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
+#             opts.append(cpp_flag(self.compiler))
+#             if has_flag(self.compiler, '-fvisibility=hidden'):
+#                 opts.append('-fvisibility=hidden')
+#         elif ct == 'msvc':
+#             opts.append('/DVERSION_INFO=\\"%s\\"' % self.distribution.get_version())
+#         for ext in self.extensions:
+#             ext.extra_compile_args = opts
+#         build_ext.build_extensions(self)
 
 setup(
     name='familiapy',
@@ -130,8 +130,8 @@ setup(
     description='Familia python binding using pybind11',
     license = "MIT",
     url = "https://github.com/bung87/familiapy", 
-    cmdclass={
-        'build_ext': BuildExt,
-    },
+    # cmdclass={
+    #     'build_ext': BuildExt,
+    # },
     ext_modules=ext_modules,
 )
